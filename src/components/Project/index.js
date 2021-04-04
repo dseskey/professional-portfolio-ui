6a65b65fc1dd458106641ff07001c9e74ac78afd
seskey <Seskey@Einstein.local>
2021-04-04 10:54:26 -0400

    Began implementation of cards on the Profile page. The number of cards are being driven by a JSON file that will later be replaced with a service backend call

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Container, Row, Col, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import projectJson from '../../assets/json/projects.json';
+function Project() {
+    return (
+        <Container fluid data-testid='project-container' id='project-container' className='pagesParentContainer'>
+            <Row className="pageHeader">
+                <h2 data-testid='project-section-header'>My Projects</h2>
+            </Row>
+            <Row>
+                {projectJson.projects.map((project) => {
+                    return (
+                        <Col data-testid={`${project.title}-col`} key={project.title + '-col'}>
+                            <Card data-testid={`${project.title}-card`} key={project.title + '-card'} style={{ width: '18rem' }}>
+                                {project.imageSRC &&
+                                    <Card.Img  data-testid={`${project.title}-card-image`} key={project.title + '-card-image'} variant="top" src={project.imageSRC} />
+                                }
+                                <Card.Body key={project.title + '-card-body-content'}>
+                                    {project.title &&
+                                        <Card.Title data-testid={`${project.title}-card-title`} key={project.title + '-card-title'}>{project.title}</Card.Title>
+                                    }
+                                    {project.description &&
+                                        <Card.Text data-testid={`${project.title}-card-text`} key={project.title + '-col'}>
+                                            {project.description}
+                                        </Card.Text>
 
-function Project(){
-    return(
-        <section>
-            Portfolio Section Placeholder
-        </section>
+                                    }
+                                </Card.Body>
+                                {/* <ListGroup data-testid={`${project.title}-tech-list`} key={project.title + '-tech-list'} className="list-group-flush">
+                                    <ListGroupItem>Cras justo odio</ListGroupItem>
+                                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                                </ListGroup> */}
+                                <Card.Body data-testid={`${project.title}-card-body-links`} key={project.title + '-card-body-links'}>
+                                    {project.githubURL &&
+                                        <Card.Link href={project.githubURL}>Github</Card.Link>
+                                    }
+                                    {project.appURL &&
+                                        <Card.Link href={project.appURL}>Application Page</Card.Link>
+                                    }
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
+
+        </Container>
     )
 }
 
